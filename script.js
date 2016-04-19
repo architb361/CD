fddb2dba7cd765ce2c834c41c1e2766fe5d95eee
archit bhatnagar <architb361@gmail.com>
2016-04-19 23:35:32 +0530

    script basic done

---
+++ script.js
@@ -0,0 +1,33 @@
+var grammer = new Array();
+
+function validate() 
+{
+    var input = document.getElementById("inp").value;
+    alert(typeof(input));
+    var state = input.split('\n');
+    var j;
+    
+    for(j=0;j<state.length;j++)
+    {
+        var line = state[j];
+        var exp = line.split("|");
+        var i;
+
+        var stm = new Object();
+
+        stm.id = line[0];
+        stm.gives = new Array();
+
+        var spli = exp[0].split('→');
+        stm.gives.push(spli[1]);
+
+        for (i = 1; i < exp.length; i++) 
+        {
+            stm.gives.push(exp[i]);    
+        }
+        for(i=0; i<stm.gives.length;i++)
+            alert(stm.gives[i]);
+
+        grammer.push(stm);
+    }
+}
